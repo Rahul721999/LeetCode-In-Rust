11d4d8adbae9a588757e965e1f99fe792a1affda
Rahul721999 <rrmondal12@gmail.com>
2023-09-16 13:11:36 +0530

    leetcode_15: 3sum

--- leetcode_15/src/main.rs
+++ leetcode_15/src/main.rs
@@ -3,12 +3,7 @@ use std::{panic, vec};
 
 fn main() {
     assert_eq!(Solution::three_sum(vec![0, 0, 0]), vec![vec![0, 0, 0]]);
-    let res = Solution::three_sum(vec![0, 1, 1]);
-    if res.is_empty() {
-        ..
-    } else {
-        panic!("Result: {:?}", res);
-    };
+    assert_eq!(Solution::three_sum(vec![0, 1, 1]), Vec::<Vec<i32>>::new());
 
     let result = Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]);
     if result == [[-1, -1, 2], [-1, 0, 1]] {
@@ -22,14 +17,14 @@ fn main() {
 
 struct Solution;
 impl Solution {
-    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> { // [-1, 0, 1, 2, -1, -4]
         let len = nums.len();
         if len < 3 {
             return vec![];
         }
-        nums.sort();
+        nums.sort(); // [-1, -1, 0, 1, 2, 4]
         let mut res_set = HashSet::new();
-        for i in 0..len {
+        for i in 0..len -2 {
             let (mut j, mut k) = (i + 1, len - 1);
             while j < k {
                 match nums[i] + nums[j] + nums[k] {
