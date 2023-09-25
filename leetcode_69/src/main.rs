1fc22db31e183ff313beb577010ad0cf5e60c889
Rahul721999 <rrmondal12@gmail.com>
2023-09-25 22:41:01 +0530

    leetcode_69: sqrt(x)

--- leetcode_69/src/main.rs
+++ leetcode_69/src/main.rs
@@ -7,22 +7,27 @@ fn main() {
 pub struct Solution;
 impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
-        // simple binary search approach..
-        let (mut l,mut r) = (0,x);
-        let mut res = 0;
-        while l <= r{
-            let mid = l + (r-l) / 2;
-            let quotient = x/mid;
-            
+        if x <= 1 {
+            return x;
+        }
+    
+        let (mut left, mut right) = (1, x);
+        let mut result = 0;
+    
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            let quotient = x / mid;
+    
             if quotient == mid {
                 return mid;
             } else if quotient < mid {
-                r = mid - 1;
+                right = mid - 1;
             } else {
-                l = mid + 1;
-                res = mid;
+                left = mid + 1;
+                result = mid;
             }
         }
-        return res;
+    
+        result
     }
 }
