f78e980a81981084c8391252be1d8465422ec057
Rahul721999 <rrmondal12@gmail.com>
2023-09-27 12:42:28 +0530

    leetcode_10: Regex pattern matching(DP-Solution)

--- leetcode_10/src/main.rs
+++ leetcode_10/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 mod tests;
 
 fn main() {
@@ -39,6 +41,49 @@ impl Solution {
             return first_match && Solution::is_match(s[1..].to_string(), p[1..].to_string());
         }
 
+    }
+
+    ///
+    /// Dynamic Programming solution with memoization...
+    /// 
+    pub fn regex(s : String, p: String) -> bool{
+        let (m, n) = (s.len(), p.len());
+
+        let s_str: Vec<char> = vec!['?'].into_iter().chain(s.chars()).collect();
+        let p_str: Vec<char> = vec!['?'].into_iter().chain(p.chars()).collect();
+        
+        // create a matrix to store the results
+        let mut dp = vec![vec![false; n+1];m+1];
+        dp[0][0] = true;
+
+        for i in 0..=m{
+            for j in 1..=n{
+                
+                if i == 0 && p_str[j] == '*'{
+                    dp[i][j] = dp[i][j-2];
+                }
+                if i == 0{continue;}
+
+                // if s[i] ==p[j] || '.'
+                if s_str[i] == p_str[j] || p_str[j] == '.'{
+                    // if s[0..i-1] matches with p[0..j-1] and the curr char also match
+                    // then dp[i][j] can be set to true.
+                    dp[i][j] = dp[i-1][j-1];
+                }else if p_str[j] == '*'{
+                    // for zero occurrence
+                    if dp[i][j-2]{
+                        dp[i][j] = dp[i][j-2];
+                    }
+                    // for multiple occurrence
+                    else if s_str[i] == p_str[j-1] || p_str[j-1] == '.'{
+                        dp[i][j] = dp[i-1][j];
+                    } 
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n]
     }
 }
 
--- leetcode_10/src/tests.rs
+++ leetcode_10/src/tests.rs
@@ -2,61 +2,61 @@
 use crate::Solution;
 #[test]
 fn test1(){
-     let res = Solution::is_match("aa".to_string(), "a".to_string());
+     let res = Solution::regex("aa".to_string(), "a".to_string());
      assert_eq!(res, false);
 }
 #[test]
 fn test2(){
-     let res = Solution::is_match("aa".to_string(), "a*".to_string());
+     let res = Solution::regex("aa".to_string(), "a*".to_string());
      assert_eq!(res, true);
 }
 #[test]
 fn test3(){
-     let res = Solution::is_match("ab".to_string(), ".*".to_string());
+     let res = Solution::regex("ab".to_string(), ".*".to_string());
      assert_eq!(res, true);
 }
 #[test]
 fn test4(){
-     let res = Solution::is_match("aab".to_string(), "c*a*b".to_string());
+     let res = Solution::regex("aab".to_string(), "c*a*b".to_string());
      assert_eq!(res, true);
 }
 #[test]
 fn test5(){
-     let res = Solution::is_match("mississippi".to_string(), "mis*is*p*.".to_string());
+     let res = Solution::regex("mississippi".to_string(), "mis*is*p*.".to_string());
      assert_eq!(res, false);
-     let res = Solution::is_match("mississippi".to_string(), "mis*is*ip*.".to_string());
+     let res = Solution::regex("mississippi".to_string(), "mis*is*ip*.".to_string());
      assert_eq!(res, true);
 }
 #[test]
 fn test6(){
-     let res = Solution::is_match("".to_string(), "a*b*".to_string());
+     let res = Solution::regex("".to_string(), "a*b*".to_string());
      assert_eq!(res, true);
 }
 
 #[test]
-    fn test_is_match() {
+    fn test_regex() {
         assert_eq!(
-            Solution::is_match(String::from("abc"), String::from("abc")),
+            Solution::regex(String::from("abc"), String::from("abc")),
             true
         );
         assert_eq!(
-            Solution::is_match(String::from("abc"), String::from("a.c")),
+            Solution::regex(String::from("abc"), String::from("a.c")),
             true
         );
         assert_eq!(
-            Solution::is_match(String::from("abc"), String::from("a.d")),
+            Solution::regex(String::from("abc"), String::from("a.d")),
             false
         );
         assert_eq!(
-            Solution::is_match(String::from("abc"), String::from("c*a.c")),
+            Solution::regex(String::from("abc"), String::from("c*a.c")),
             true
         );
      //    assert_eq!(
-     //        Solution::is_match(String::from("a"), String::from(".*..a*")),
+     //        Solution::regex(String::from("a"), String::from(".*..a*")),
      //        false
      //    );
         assert_eq!(
-            Solution::is_match(String::from("a"), String::from("")),
+            Solution::regex(String::from("a"), String::from("")),
             false
         );
     }
