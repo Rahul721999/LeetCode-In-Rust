ef4e2f83e2d2da8cdd0686b0f7daa9b4602721ea
Rahul721999 <rrmondal12@gmail.com>
2023-10-13 13:11:43 +0530

    leetcode_912: QuickSort Implemented

--- leetcode_912/src/main.rs
+++ leetcode_912/src/main.rs
@@ -1,53 +1,33 @@
+mod tests;
 fn main() {
-    let res = Solution::quick_sort_array(vec![4, 1, 3, 6, 9, 3, 2, 5].as_mut());
+    let res = Solution::quick_sort_array(vec![5,1,0,0]);
     println!("{:?}", res);
 }
 
 struct Solution;
 impl Solution {
-    pub fn quick_sort_array(arr: &mut Vec<i32>) -> Vec<i32> {
-        let len = arr.len();
-        if len <= 2 {
-            return arr.to_vec();
+    pub fn quick_sort_array(mut nums: Vec<i32>) -> Vec<i32> {
+        quick_sort(&mut nums[..]);
+        return nums;
+        fn quick_sort(nums: &mut [i32]) {
+            let len = nums.len();
+            if len < 2 {
+                return;
+            }
+            let pivot = nums[(len-1) / 2];
+            let (mut l, mut r) = (0, len+1);
+            let partition_point = loop {
+                while { l+=1; nums[l-1] < pivot } {}
+                while { r-=1; nums[r-1] > pivot } {}
+                if r <= l {
+                    break r-1;
+                }
+                nums.swap(l-1, r-1);
+            };
+            quick_sort(&mut nums[0..=partition_point]);
+            quick_sort(&mut nums[partition_point+1..]);
         }
-        // partitioning index
-        let p_idx = partition(&mut arr.to_vec());
-        let mut res = Vec::new();
-        if p_idx > 0 {
-            let mut left_sorted_arr = Solution::quick_sort_array(&mut arr[0..p_idx].to_vec());
-            // arr[0..p_idx].copy_from_slice(&left_sorted_arr);
-            res.append(&mut left_sorted_arr);
-        }
-        if p_idx < len - 1 {
-            // sort the right array to the pivot ele
-            let mut right_sorted_arr = Solution::quick_sort_array(&mut arr[p_idx + 1..len].to_vec());
-            // arr[p_idx + 1..len].copy_from_slice(&right_sorted_arr);
-            res.append(&mut right_sorted_arr);
-        }
-        res
+        
     }
 }
-pub fn partition(arr: &mut Vec<i32>) -> usize {
-    let len = arr.len();
-    let p_idx = len - 1;
-    let (mut l_ptr, mut r_ptr) = (0, p_idx - 1);
-    while l_ptr <= r_ptr {
-        while arr[l_ptr] < arr[p_idx] {
-            l_ptr += 1;
-        }
-        while arr[r_ptr] > arr[p_idx] {
-            r_ptr -= 1;
-        }
-        // if arr[l_ptr] > arr[r_ptr]{
-        if l_ptr < r_ptr {
-            // println!("swapping: {} -> {}", arr[l_ptr], arr[r_ptr]);
-            arr.swap(l_ptr, r_ptr);
-            l_ptr += 1;
-            r_ptr -= 1;
-        }
-    }
-    arr.swap(l_ptr, p_idx);
 
-    //return new pivot index
-    l_ptr
-}
