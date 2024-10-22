e7a56a2b58a24729ae33fc4506aa961acc6c856e
Rahul721999 <rrmondal12@gmail.com>
2024-10-22 10:41:23 +0530

    update: leetcode.sh script

--- leetcode.sh
+++ leetcode.sh
@@ -69,8 +69,13 @@ echo -e "${GREEN}Created ${YELLOW}$FILE_NAME${GREEN} in the ${YELLOW}$CURRENT_DI
 # Add "mod tests;" in main.rs line 1
 MAIN_RS_FILE="main.rs"
 
-# Use sed to insert "mod tests;" at the first line
+# insert "mod tests;" at the first line
 sed -i '1imod tests;' "$MAIN_RS_FILE"
+
+# insert "pub struct Solution;" at 5th line
+sed -i '$apub struct Solution;' "$MAIN_RS_FILE"
 
 # Notify the user that the line has been added
 echo -e "${GREEN}Added ${YELLOW}mod tests;${GREEN} to ${YELLOW}$MAIN_RS_FILE${GREEN} at line 1.${NC}"
+
+cat "$MAIN_RS_FILE"
