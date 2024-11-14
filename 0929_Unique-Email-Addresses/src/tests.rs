e27003ba33f542c4c4a8f65bb0e6350ba9c5cb7e
Rahul721999 <rrmondal12@gmail.com>
2024-11-14 11:11:11 +0530

    leetcode_929: Unique Email Addresses (Optimized Solution)

--- 0929_Unique-Email-Addresses/src/tests.rs
+++ 0929_Unique-Email-Addresses/src/tests.rs
@@ -32,3 +32,35 @@ fn test3() {
     let output = Solution::num_unique_emails(intput);
     assert_eq!(output, 3);
 }
+
+#[test]
+fn test4() {
+    let intput = vec![
+        "test.email+alex@leetcode.com".to_string(),
+        "test.e.mail+bob.cathy@leetcode.com".to_string(),
+        "testemail+david@lee.tcode.com".to_string(),
+    ];
+    let output = Solution::optimized(intput);
+    assert_eq!(output, 2);
+}
+
+#[test]
+fn test5() {
+    let intput = vec![
+        "test.email.leet+alex@code.com".to_string(),
+        "test.email+alex@leetcode.com".to_string(),
+    ];
+    let output = Solution::optimized(intput);
+    assert_eq!(output, 2);
+}
+
+#[test]
+fn test6() {
+    let intput = vec![
+        "a@leetcode.com".to_string(),
+        "b@leetcode.com".to_string(),
+        "c@leetcode.com".to_string(),
+    ];
+    let output = Solution::optimized(intput);
+    assert_eq!(output, 3);
+}
