0c18b8d4e61bd45ce8bb3ab50758cf6bb31c4c85
Rahul721999 <rrmondal12@gmail.com>
2024-11-18 18:36:44 +0530

    leetcode_387: First unique char in string (Optimized)

--- 0387_First-Unique-Char-in-String/src/main.rs
+++ 0387_First-Unique-Char-in-String/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+
 
 mod tests;
 fn main() {
@@ -7,7 +7,26 @@ fn main() {
 }
 pub struct Solution;
 impl Solution {
-    pub fn first_uniq_char(s: String) -> i32 {
+    pub fn first_uniq_char(s: String) -> i32{
+        let mut v = vec![0;26];
+        
+        for char in s.chars(){
+            v[char as usize - 97] += 1;
+        }
+
+        let mut index = 0;
+        for ele in s.chars(){
+            if v[ele as usize - 97] == 1{
+                return index;
+            }
+            index +=1 ;
+        }
+        // if unique value not present return -1
+        -1
+    }
+
+    pub fn first_uniq_char_unoptimized_solution(s: String) -> i32 {
+        use std::collections::HashMap;
         let mut map : HashMap<_, _>= HashMap::new();
 
         for ele in s.chars(){
