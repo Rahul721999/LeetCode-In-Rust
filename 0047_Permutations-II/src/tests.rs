401d8afb579cbb5306ad7e59422c29bb4d816957
Rahul721999 <rrmondal12@gmail.com>
2025-01-15 22:03:15 +0530

    leetcode_47: Permutations-II (Test)

--- 0047_Permutations-II/src/tests.rs
+++ 0047_Permutations-II/src/tests.rs
@@ -3,15 +3,52 @@ use crate::*;
 
 #[test]
 fn test1() {
-    // Add test here
+    let input = vec![1, 1, 2];
+    let output = Solution::permute_unique(input);
+    let expected = vec![vec![1, 1, 2], vec![1, 2, 1], vec![2, 1, 1]];
+
+    expected.iter().for_each(|v| {
+        if !output.contains(v) {
+            panic!("Missing permutation: {v:?}")
+        }
+    });
+
+    assert_eq!(output.len(), expected.len(), "Output length mismatch");
 }
 
 #[test]
 fn test2() {
-    // Add test here
+    let input = vec![1, 2, 3];
+    let output = Solution::permute_unique(input);
+    let expected = vec![
+        vec![1, 2, 3],
+        vec![1, 3, 2],
+        vec![2, 1, 3],
+        vec![2, 3, 1],
+        vec![3, 1, 2],
+        vec![3, 2, 1],
+    ];
+
+    expected.iter().for_each(|v| {
+        if !output.contains(v) {
+            panic!("Missing permutation: {v:?}")
+        }
+    });
+
+    assert_eq!(output.len(), expected.len(), "Output length mismatch");
 }
 
 #[test]
 fn test3() {
-    // Add test here
+    let input = vec![1, 1, 1];
+    let output = Solution::permute_unique(input);
+    let expected = vec![vec![1, 1, 1]];
+
+    expected.iter().for_each(|v| {
+        if !output.contains(v) {
+            panic!("Missing permutation: {v:?}")
+        }
+    });
+
+    assert_eq!(output.len(), expected.len(), "Output length mismatch");
 }
